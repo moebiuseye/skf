8e06f94b789337ce8d8c583004662f82056fa409
moebiuseye <moebius.eye@gmail.com>
2013-09-30 14:13:26 +0200

    fixed "just_copy" plugin bug

--- plugins/just_copy.sh
+++ plugins/just_copy.sh
@@ -4,11 +4,8 @@
 t_skf_gen () {
     echo "You are using an untested plugin. Please report bugs "
     echo "to moebiuseye <moebiuseye@jeannedhack.org>"
-    find "$SRC" -maxdepth 1 -type d | while read d 
-    do 
-        if ! [ -f "$d/.skfrc" ] || [ "$d" == "$SRC" ]
-        then
-            cp -R -- "$d" "$DST${d#$SRC}" 
-        fi
-    done
+    
+    rm -rf "$DST"
+    cp -r -- "$SRC" "$DST"
+    rm "$DST/.skfrc"
 }
